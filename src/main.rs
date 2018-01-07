f14531326b39c0ae01ccaa2d0a696772ea763e6f
Michael McKinnon <michael@clicknames.com.au>
2018-01-07 20:37:41 +1100

    Creates a bitcoin address from sha256('1972')

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 extern crate bitcoin;
 extern crate secp256k1;
 extern crate rand;
+extern crate crypto;
+extern crate hex;
 
 use bitcoin::util::address::{Address,Privkey};
 use secp256k1::Secp256k1;
 use secp256k1::key::SecretKey;
 use bitcoin::network::constants::Network;
-use rand::thread_rng;
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
 
 fn main() {
 	let secp = Secp256k1::new();
-	let mut rng = thread_rng();
-	let mysecret = SecretKey::new(&secp, &mut rng);
+	let mut hasher = Sha256::new();
+	hasher.input_str("1972");
+	let mut newhash: [u8; 32] = [0; 32];
+	hasher.result(&mut newhash);
+	let mysecret = match SecretKey::from_slice(&secp, &newhash[0..32]) {
+		Ok(key) => { key },
+		Err(e) => { panic!("{}", e) }
+	};
 	let myprivkey = Privkey::from_key(Network::Bitcoin, mysecret, false);
 	let myaddress: Address = myprivkey.to_address(&secp).unwrap();
 
